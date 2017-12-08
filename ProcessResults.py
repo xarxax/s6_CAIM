8fc3ef51f3368ad72fc2925cdc1c714f6d734e11
victor-gonzalez-prieto <victor.gonzalez.prieto+git@gmail.com>
2017-12-08 18:27:26 +0100

    ProcessResults updated

--- ProcessResults.py
+++ ProcessResults.py
@@ -22,22 +22,36 @@ ProcessPrototype
 """
 
 from __future__ import print_function, division
-
 import argparse
+from operator import itemgetter
 
 __author__ = 'bejar'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--prot', default='prototypes-final.txt', help='prototype file')
+    parser.add_argument('--assi', default='assignments-final.txt', help='assignments file')
     parser.add_argument('--natt', default=5, type=int, help='Number of attributes to show')
     args = parser.parse_args()
 
     f = open(args.prot, 'r')
-
     for line in f:
         cl, attr = line.split(':')
         print(cl)
         latt = sorted([(float(at.split('+')[1]), at.split('+')[0]) for at in attr.split()], reverse=True)
         print(latt[:args.natt])
+    f.close()
+    
+    f = open(args.assi, 'r')
+    for line in f:
+        cl, attr = line.split(':')
+        docs = attr.split()
+        groups = [doc.split('/')[0] for doc in docs]
+        gcount = [(g, groups.count(g)) for g in set(groups)]
+        gcount = sorted(gcount,reverse=True,key=itemgetter(1))
+        print(cl)
+        print(gcount)
+    f.close()
+    
+
 
